6cbd198986540e3b73aa72d5c2366216c747e0a2
Alex <35180934+Artnoc1@users.noreply.github.com>
2022-04-28 23:23:39 +0300

    rename arch-install-all-packages.sh

--- arch-install-all-packages.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-# archboot is fucked
-while IFS='' read -r line || [[ -n "$line" ]]; do
-    package-query -S --qconflicts "$line"
-done < <(pacman -Slq core extra community | grep -v archboot)
-
